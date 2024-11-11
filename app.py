5c6bc1de577cffe36ea7461f115e9c0e2a3f60c6
szhang655 <61028640+szhang655@users.noreply.github.com>
2024-11-11 12:55:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -295,6 +295,7 @@ app_ui = ui.page_sidebar(
         ui.value_box("Great Circle Distance", ui.output_text("great_circle_dist"), theme="gradient-blue-indigo", showcase=icon_svg("globe")),
         ui.value_box("Estimated Arrival Time", ui.output_text("arrival_time_output"), theme="gradient-blue-indigo", showcase=icon_svg("plane")),
         ui.value_box("Destination Airport Weather", ui.output_text("weather_info"), theme="gradient-blue-indigo", showcase=icon_svg("cloud")),
+        fill = False,
         height="220px",
     ),
     ui.card(
